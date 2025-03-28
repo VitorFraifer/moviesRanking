4d3cc3832f41f4155c9f2996d8c30bd5af766c77
Vitor Fraifer <vitorfraifer10@gmail.com>
2025-03-27 21:47:47 -0300

    fix: ajeitando menu sanduiche, estilo + logica

--- static/js/menu-sanduiche.js
+++ static/js/menu-sanduiche.js
@@ -1,12 +1,16 @@
 const btnFecharModal = document.querySelector(".fechar-nav-button");
 const navMobile = document.querySelector(".menu-sanduiche-container ul");
-const abrirModal = document.querySelector(".menu-sanduiche-container img")
+const abrirModal = document.querySelector(".menu-sanduiche-container img");
+const modalShadow = document.querySelector(".modal-shadow");
 
 //Fechar navbar mobile
 btnFecharModal.addEventListener("click", () => {
     navMobile.style.display = "none";
+    modalShadow.style.display = "none"
 })
 
+//Abrir navbar mobile
 abrirModal.addEventListener("click", () => {
     navMobile.style.display = "flex";
+    modalShadow.style.display = "block"
 })
